7310cbe904f1667cfda4d9e1f31044aedbfd916c
Romário Abílio <romarioabilio22082001@gmail.com>
2024-02-04 10:39:46 -0300

    Ajustando validate de telefone

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -4,18 +4,8 @@ class Cliente < ApplicationRecord
 
   # Validations
   validates :nome, presence: true, length: { minimum: 2, maximum: 100 }
-  validates :telefone, presence: true, length: { minimum: 10, maximum: 15 }, format: { with: /\A\+?\d{2}\d{8,9}\z/, message: "deve começar com DDD e ter 8 ou 9 dígitos para o número" }
+  validates :telefone, presence: true, format: { with: /\A\(\d{2}\)\d{5}-\d{4}|\d{11}\z/, message: "deve estar no formato (DDD)99999-9999 ou DDD99999999" }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "formato inválido" }
   validates :cpf, presence: true, uniqueness: true, length: { is: 11 }, numericality: { only_integer: true }, format: { with: /\A\d{11}\z/, message: "deve ter 11 dígitos" }
   validates :idade, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 120 }
-
-  validate :cpf_valido
-
-  private
-
-  def cpf_valido
-    unless CPF.valid?(cpf)
-      errors.add(:cpf, "não é válido")
-    end
-  end
 end
