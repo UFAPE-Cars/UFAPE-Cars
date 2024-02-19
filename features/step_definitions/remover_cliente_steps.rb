85554a8b1acb954d59b1ae3fca198b235fef2957
Romário Abílio <romarioabilio22082001@gmail.com>
2024-02-18 22:48:34 -0300

    Cenário 4 , removendo um cliente

---
+++ features/step_definitions/remover_cliente_steps.rb
@@ -0,0 +1,19 @@
+Given('O cliente com o nome {string}, telefone {string}, email {string}, cpf {string}, idade {int} existe') do |nome, telefone, email, cpf, idade|
+  visit '/clientes/new'
+  expect(page).to have_current_path('/clientes/new')
+  fill_in 'Nome', with: nome
+  fill_in 'Telefone', with: telefone
+  fill_in 'Email', with: email
+  fill_in 'Cpf', with: cpf
+  fill_in 'Idade', with: idade
+  click_button 'Create Cliente'
+  expect(page).to have_content('Cliente was successfully created.')
+end
+
+When('Eu clico em remover cliente') do
+  click_button 'Destroy this cliente'
+end
+
+Then('Eu vejo uma mensagem de cliente removido com sucesso') do
+  expect(page).to have_content('Cliente was successfully destroyed.')
+end
