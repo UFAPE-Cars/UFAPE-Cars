adb71cc2bd7d44a3ffa14bf796a4a14ca0645cb3
Romário Abílio <romarioabilio22082001@gmail.com>
2024-02-18 22:44:17 -0300

    Cenário 1 , registrando cliente válido

---
+++ features/step_definitions/registrar_cliente_valido_steps.rb
@@ -0,0 +1,21 @@
+Given('Estou na pagina de registrar um cliente') do
+  visit '/clientes/new'
+  expect(page).to have_current_path('/clientes/new')
+end
+
+When('Eu preencho os campos de registro com nome {string}, telefone {string}, email {string}, cpf {string}, idade {int}') do |nome, telefone, email, cpf, idade|
+  fill_in 'Nome', with: nome
+  fill_in 'Telefone', with: telefone
+  fill_in 'Email', with: email
+  fill_in 'Cpf', with: cpf
+  fill_in 'Idade', with: idade
+
+end
+
+And('Eu clico no botao de registrar cliente') do
+  click_button 'Create Cliente'
+end
+
+Then('Eu vejo uma mensagem de cliente registrado') do
+  expect(page).to have_content('Cliente was successfully created.')
+end
