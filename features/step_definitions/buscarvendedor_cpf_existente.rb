0459a6751d4c0ad3f2ecaa926ee6f8fd79b8d1b8
FelipeMendes1 <felipe.mssilva@ufpe.br>
2024-02-19 21:32:55 -0300

    adicionando algumas informações básicas

--- features/step_definitions/buscarvendedor_cpf_existente.rb
+++ features/step_definitions/buscarvendedor_cpf_existente.rb
@@ -4,6 +4,4 @@ end
 
 Then('o sistema retorna os detalhes do vendedor {string}') do |nome_esperado|
   expect(page).to have_content(nome_esperado)
-  # Adicione mais verificações conforme necessário para confirmar os detalhes do vendedor
-  # Por exemplo, verificar se o CPF ou outras informações relevantes do vendedor estão sendo exibidas
 end
