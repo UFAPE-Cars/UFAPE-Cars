a0336df91c1b5105a7c4fd3468d732f1d3a724c6
brendobrito2002 <brendoplayer@gmail.com>
2024-02-26 15:15:15 -0300

    Refactoring - Extract Method em veiculos controller, em create e update

--- app/controllers/veiculos_controller.rb
+++ app/controllers/veiculos_controller.rb
@@ -55,27 +55,19 @@ class VeiculosController < ApplicationController
   def create
     @veiculo = Veiculo.new(veiculo_params)
 
-    respond_to do |format|
-      if @veiculo.save
-        format.html { redirect_to veiculo_url(@veiculo), notice: "Veiculo was successfully created." }
-        format.json { render :show, status: :created, location: @veiculo }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @veiculo.errors, status: :unprocessable_entity }
-      end
+    if @veiculo.save
+      responde_to_veiculo(@veiculo, :created, "Veiculo was successfully created.")
+    else
+      respond_with_errors(:new)
     end
   end
 
   # PATCH/PUT /veiculos/1 or /veiculos/1.json
   def update
-    respond_to do |format|
-      if @veiculo.update(veiculo_params)
-        format.html { redirect_to veiculo_url(@veiculo), notice: "Veiculo was successfully updated." }
-        format.json { render :show, status: :ok, location: @veiculo }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @veiculo.errors, status: :unprocessable_entity }
-      end
+    if @veiculo.update(veiculo_params)
+      responde_to_veiculo(@veiculo, :created, "Veiculo was successfully updated.")
+    else
+      respond_with_errors(:edit)
     end
   end
 
@@ -99,4 +91,21 @@ class VeiculosController < ApplicationController
     def veiculo_params
       params.require(:veiculo).permit(:modelo, :ano, :quilometragem, :uso, :historico_manutencao, :tabela_fipe, :valor_anuncio)
     end
+
+
+  # Metodo refatorado de "respond_to" de create e update (sucesso)
+  def responde_to_veiculo(veiculo, status, notice)
+    respond_to do |format|
+      format.html { redirect_to veiculo_url(veiculo), notice: notice }
+      format.json { render :show, status: status, location: veiculo }
+    end
+  end
+
+  # Metodo refatorado de "respond_to" de create e update (erro)
+  def respond_with_errors(action)
+    respond_to do |format|
+      format.html { render action, status: :unprocessable_entity }
+      format.json { render json: @veiculo.errors, status: :unprocessable_entity }
+    end
+  end
 end
