081fda67ad8e368db9086ef6bec1f8eac94adbd3
Gabriel <josegabrielpereira2017@gmail.com>
2024-02-27 14:51:54 -0300

    Implementando o Devise como mecanismo de autentificação de nosso sistema. Telas de login e cadastro adicionadas.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :usuarios, controllers: {
+    sessions: 'usuarios/sessions'
+  }
+
   resources :vendas
   resources :clientes
   resources :veiculos do
@@ -13,5 +17,5 @@ Rails.application.routes.draw do
   end
 
   # Defines the root path route ("/")
-  root "render#index"
+  root "clientes#index"
 end
